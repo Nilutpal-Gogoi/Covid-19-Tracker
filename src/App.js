4696eb57a05e252020a0bcb5a554cb1a027573b1
nilutpal <nilutpal.ee@gmail.com>
2020-11-07 21:50:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core"; 
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  // STATE = How to write a variable in REACT <<<<<<<<<<<<<<<<<<<<<
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">  {/* BEM naming convention */}
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant = "outlined" value = "abc">
+            {/* Loop through all the countries and show a drop down list of the options*/}
+
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option Two</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">Yoooo</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+      
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+      </div>
   );
 }
 
